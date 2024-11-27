01f5ae8a6352aecab1f8d508cac4a33ea1f56351
Filip Lajszczak <filip@lajszczak.dev>
2024-11-27 19:23:23 +0000

    makes settings and manage more modern.

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from pathlib import Path
 from typing import List
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
